e15cb6823e52cac935b0cce522d0260b69ae3370
sokhuong uon <sokhuong.usk@gmail.com>
2022-05-10 22:29:02 +0700

    ✨ added spot light

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Text, OrbitControls } from '@react-three/drei'
+import { Text, OrbitControls, SpotLight } from '@react-three/drei'
 
 const IntroMessage = () => {
 	return (
 		<Text
 			rotation={[-Math.PI / 6, -Math.PI / 6, 0]}
-			fontSize={12}
-			maxWidth={110}
+			fontSize={1}
+			maxWidth={10}
 			lineHeight={1}
 			letterSpacing={0.02}
 			textAlign={'left'}
@@ -25,9 +25,19 @@ const IntroMessage = () => {
 
 const Intro = () => {
 	return (
-		<Canvas camera={{ position: [0, 0, 90] }}>
+		<Canvas camera={{ position: [0, 0, 10], zoom: 1 }}>
 			<ambientLight />
 			<OrbitControls />
+			<SpotLight
+				position={[-3, -2, 8]}
+				color={'yellow'}
+				intensity={5}
+				penumbra={1}
+				distance={10}
+				angle={0.8}
+				attenuation={10}
+				anglePower={4}
+			></SpotLight>
 			<Suspense fallback={null}>
 				<IntroMessage />
 			</Suspense>
