6dd6118ea79492261d343c12cb3407c4d0de3fae
Sokhuong Uon <sokhuong.usk@gmail.com>
2022-12-30 20:56:28 +0700

    fix: disable x-powered-by

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	output: 'standalone'
+	output: 'standalone',
+	poweredByHeader: false
 }
 
 module.exports = nextConfig
