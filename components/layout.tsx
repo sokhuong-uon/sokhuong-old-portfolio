5713563c1a6173e82eaac0a47307b59965c9b439
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-07-15 14:25:34 +0700

    chore(dep): update nextjs codemod

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import { Poppins } from '@next/font/google'
+import { Poppins } from 'next/font/google'
 import { PropsWithChildren } from 'react'
 import { Dock } from './dom/dock/Dock'
 import { ThreeDContent } from './r3f'
